63c1144f0aff141e4a68f85badcf0589b03d4022
Balázs Faludi <balazsfaludi@gmail.com>
2020-01-21 21:32:37 +0100

    Initial commit

---
+++ Assets/CameraDepthNormals.shader
@@ -0,0 +1,81 @@
+﻿Shader "CustomCameraMode/CameraDepthNormals"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    
+    CGINCLUDE
+        
+        #include "UnityCG.cginc"
+        
+        sampler2D _MainTex;
+        float4 _MainTex_ST;
+        
+        struct appdata
+        {
+            float4 vertex : POSITION;
+            float2 uv : TEXCOORD0;
+        };
+        
+        struct v2f
+        {
+            float2 uv : TEXCOORD0;
+            float4 vertex : SV_POSITION;
+        };
+        
+        v2f vert (appdata v)
+        {
+            v2f o;
+            o.vertex = UnityObjectToClipPos(v.vertex);
+            o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+            return o;
+        }
+        
+    ENDCG
+    
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {        
+            Name "CameraDepthBlit"
+            
+            CGPROGRAM
+            
+                #pragma vertex vert
+                #pragma fragment frag
+    
+                fixed4 frag (v2f i) : SV_Target
+                {
+                    float depth = tex2D(_MainTex, i.uv).r;
+                    depth = Linear01Depth(depth);
+                    return float4(depth, depth, depth, 1);
+                }
+
+            ENDCG
+        }
+        
+        Pass
+        {        
+            Name "CameraNormalsBlit"
+            
+            CGPROGRAM
+            
+                #pragma vertex vert
+                #pragma fragment frag
+    
+                fixed4 frag (v2f i) : SV_Target
+                {
+                    float depth;
+                    float3 normal;
+                    DecodeDepthNormal(tex2D(_MainTex, i.uv), depth, normal);
+                    return float4(normal.r, normal.g, normal.b, 1);
+                }
+
+            ENDCG
+        }
+    }
+}
---
+++ Assets/CustomCameraMode.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// This static class adds a depth and a normals camera mode to the scene view.
+/// </summary>
+public static class CustomCameraMode
+{
+    static SceneView.CameraMode depthCameraMode;
+    static SceneView.CameraMode normalsCameraMode;
+    
+    static Dictionary<Camera, CommandBuffer> commandBuffers = new Dictionary<Camera, CommandBuffer>();
+
+    static int depthBlitPass;
+    static int normalsBlitPass;
+    static Material blitMaterial;
+    static Material BlitMaterial
+    {
+        get
+        {
+            if (blitMaterial == null)
+            {
+                blitMaterial = new Material(Shader.Find("CustomCameraMode/CameraDepthNormals"));
+                depthBlitPass = blitMaterial.FindPass("CameraDepthBlit");
+                normalsBlitPass = blitMaterial.FindPass("CameraNormalsBlit");
+            }
+            return blitMaterial;
+        }
+    }
+
+    [InitializeOnLoadMethod]
+    static void Initialize()
+    {
+        depthCameraMode = SceneView.AddCameraMode("Camera Depth", "Shading Mode");
+        normalsCameraMode = SceneView.AddCameraMode("Camera Normals", "Shading Mode");
+        Camera.onPreRender += OnPreRender;
+    }
+    
+    static void OnPreRender(Camera cam)
+    {
+        var sceneView = SceneView.currentDrawingSceneView;
+        if (sceneView != null)
+        {
+            if (sceneView.cameraMode == depthCameraMode)
+            {
+                cam.depthTextureMode |= DepthTextureMode.Depth;
+                UpdateCommandBuffer(cam, depthCameraMode);
+            }
+            else if (sceneView.cameraMode == normalsCameraMode)
+            {
+                cam.depthTextureMode |= DepthTextureMode.DepthNormals;
+                UpdateCommandBuffer(cam, normalsCameraMode);
+            }
+            else
+            {
+                RemoveCommandBuffer(cam);
+            }
+        }
+    }
+
+    static void UpdateCommandBuffer(Camera cam, SceneView.CameraMode mode)
+    {
+        // If this is the first time we're rendering this camera, create a command buffer and add it to the camera.
+        if (!commandBuffers.TryGetValue(cam, out var buffer))
+        {
+            buffer = new CommandBuffer();
+            buffer.name = $"Custom Camera Mode";
+            cam.AddCommandBuffer(CameraEvent.AfterForwardAlpha, buffer);
+            commandBuffers[cam] = buffer;
+        }
+        
+        // Select the right source texture and blit it into the camera target.
+        var source = mode == depthCameraMode ? BuiltinRenderTextureType.Depth : BuiltinRenderTextureType.DepthNormals;
+        int blitPass = mode == depthCameraMode ? depthBlitPass : normalsBlitPass;
+        
+        buffer.Clear();
+        buffer.Blit(source, BuiltinRenderTextureType.CameraTarget, BlitMaterial, blitPass);
+    }
+
+    static void RemoveCommandBuffer(Camera cam)
+    {
+        if (commandBuffers.TryGetValue(cam, out var buffer))
+        {
+            cam.RemoveCommandBuffer(CameraEvent.AfterForwardAlpha, buffer);
+            commandBuffers.Remove(cam);
+        }
+    }
+}
+
